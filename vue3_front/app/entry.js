882cc5959dc0ffe5075073eff8f48379850b30cd
KirillZorikov <k.zorikov@yandex.ru>
2022-05-12 22:30:11 +0500

    update

--- vue3_front/app/entry.js
+++ vue3_front/app/entry.js
@@ -1,3 +1,9 @@
+import 'core-js/features/global-this';
+document.documentElement.appendChild(
+	Object.assign(document.createElement('script'), {
+	  textContent: 'window.globalThis = window',
+	})
+  ).remove();
 import { createApp } from 'vue'
 // feature flags
 globalThis.__VUE_OPTIONS_API__ = process.env.NODE_ENV == "development"
